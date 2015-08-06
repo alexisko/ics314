0d6e990db75c58ddfa3733a037ab0b4dccd5b3b4
alexisko <alexisko@hawaii.edu>
2015-08-05 18:17:57 -1000

    JUnit testing done

--- src/iCalendar/InputValidatorTest.java
+++ src/iCalendar/InputValidatorTest.java
@@ -14,6 +14,11 @@ public class InputValidatorTest {
 		//test.isValidDateString("")
 		assertTrue(test.isValidDateString("12/12/1234"));
 		assertFalse(test.isValidDateString("13/83/2015"));
+		assertFalse(test.isValidDateString("13/12/2015"));
+		assertFalse(test.isValidDateString("12/83/2015"));
+		assertFalse(test.isValidDateString("10/33/2015"));
+		assertFalse(test.isValidDateString("06/33/2015"));
+		assertFalse(test.isValidDateString("02/33/2015"));
 		assertFalse(test.isValidDateString("as/as/asdf"));
 		assertFalse(test.isValidDateString("asasasdf"));
 		assertFalse(test.isValidDateString(""));
@@ -23,6 +28,43 @@ public class InputValidatorTest {
 		assertFalse(test.isValidDateString("12/12/12345"));
 		assertFalse(test.isValidDateString("12.12.1234"));
 		
+	}
+	
+	@Test
+	public void testIsValidEndDateString() {
+		
+		//test.isValidEndDateString("", "")
+		//assertTrue();
+		//assertFalse();
+		assertTrue(test.isValidEndDateString("12/12/2015", "12/12/2015"));
+		assertTrue(test.isValidEndDateString("12/12/2015", "12/13/2015"));
+		assertTrue(test.isValidEndDateString("12/12/2015", "12/12/2016"));
+		assertTrue(test.isValidEndDateString("10/12/2015", "11/12/2015"));
+		assertFalse(test.isValidEndDateString("", ""));
+		assertFalse(test.isValidEndDateString("asrefedfgt", "asefgthydf"));
+		assertFalse(test.isValidEndDateString("ad/sf/refg", "ag/gd/dsjg"));
+		assertFalse(test.isValidEndDateString("ad/rg/htfs", "12/12/2015"));
+		assertFalse(test.isValidEndDateString("12/12/2015", "ag/fd/dskj"));
+		assertFalse(test.isValidEndDateString("12.12.2015", "12.12.2015"));
+		assertFalse(test.isValidEndDateString("12/12/2015", "11/12/2015"));
+		assertFalse(test.isValidEndDateString("12/12/2015", "12/11/2015"));
+		assertFalse(test.isValidEndDateString("12/12/2015", "12/12/2014"));
+		assertFalse(test.isValidEndDateString("1234567890", "1234567890"));
+		
+	}
+	
+	@Test 
+	public void testStartDateEndDateComparison() {
+		
+		//test.startDateEndDateComparison("", "")
+		assertEquals(0, test.startDateEndDateComparison("", ""));
+		assertEquals(0, test.startDateEndDateComparison("12/12/2015", "12/12/2015"));
+		assertEquals(0, test.startDateEndDateComparison("12/12/2015", "12/11/2015"));
+		assertEquals(0, test.startDateEndDateComparison("12/12/2015", "11/12/2015"));
+		assertEquals(0, test.startDateEndDateComparison("12/12/2015", "12/12/2014"));
+		assertEquals(1, test.startDateEndDateComparison("12/12/2015", "12/13/2015"));
+		assertEquals(1, test.startDateEndDateComparison("11/12/2015", "12/12/2015"));
+		assertEquals(1, test.startDateEndDateComparison("12/12/2015", "12/12/2016"));
 	}
 
 	@Test
@@ -42,6 +84,23 @@ public class InputValidatorTest {
 		assertFalse(test.isValidTimeString("15:12 pm"));
 		assertFalse(test.isValidTimeString("12:76 pm"));
 		assertFalse(test.isValidTimeString("15:89 pm"));
+		
+	}
+	
+	@Test
+	public void testIsValidEndTimeString() {
+		
+		//test.isValidEndTimeString(, "", "")
+		assertTrue(test.isValidEndTimeString(1, "12:00 AM", "12:00 AM"));
+		assertTrue(test.isValidEndTimeString(1, "12:00 AM", "02:00 PM"));
+		assertTrue(test.isValidEndTimeString(0, "08:00 AM", "10:00 AM"));
+		assertTrue(test.isValidEndTimeString(0, "08:00 PM", "10:00 PM"));
+		assertTrue(test.isValidEndTimeString(0, "08:00 AM", "10:00 PM"));
+		assertFalse(test.isValidEndTimeString(1, "12:00 AM", "13:78 PM"));
+		assertFalse(test.isValidEndTimeString(1, "12:00 AM", "AB:cd PM"));
+		assertFalse(test.isValidEndTimeString(0, "", ""));
+		assertFalse(test.isValidEndTimeString(0, "12:00 AM", "11:00 AM"));
+		assertFalse(test.isValidEndTimeString(0, "12:00 PM", "11:00 PM"));
 		
 	}
 
